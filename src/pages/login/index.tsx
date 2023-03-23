1b2b15d14572d68d13da6854e17d69741e6cd65f
Gabriellimmaa <42157830+Gabriellimmaa@users.noreply.github.com>
2023-03-23 17:20:08 -0300

    fix: styles login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import styles from '../login/styles/login.module.css'
-const inter = Inter({ subsets: ['latin'] })
+import { FaMapMarkerAlt } from 'react-icons/fa'
+
 export default function Login() {
   return (
     <section className={styles.sectionContainer}>
@@ -16,25 +17,27 @@ export default function Login() {
         />
         <div className={styles.cardContainer}>
           <form className={styles.formContainer}>
-            <h1>Bem-vindo ao Login 🙌</h1>
+            <h1>Login 🙌</h1>
             <div className={styles.line} />
             <input type="text" placeholder="E-mail" />
             <input type="password" placeholder="Senha" />
             <div className={styles.rememberMeContainer}>
               <div>
-                <input type="checkbox" />
-                <label>Lembrar-me</label>
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Lembrar-me</label>
               </div>
               <Link href="#">Esqueceu a senha?</Link>
             </div>
             <button type="submit">Entrar</button>
             <div className={styles.createAccount}>
               <p>Não tem uma conta?</p>
-              <Link href="#" className={styles.span}>Cadastre-se</Link>
+              <Link href="#" className={styles.span}>
+                Cadastre-se
+              </Link>
             </div>
           </form>
         </div>
       </main>
     </section>
   )
-} 
+}
