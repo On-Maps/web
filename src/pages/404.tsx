df3bbd8ed3cdb69622513267abe5ba218d057955
Gabriellimmaa <42157830+Gabriellimmaa@users.noreply.github.com>
2023-03-24 09:13:46 -0300

    fix: refactor 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,84 +1,48 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import { flexCenterContent } from '@/utils/cssInJsBlocks'
 
 export default function Custom404() {
   return (
-    <Box sx={{ ...flexCenterContent, width: 1, height: '100vh' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          width: 1,
-          height: 1,
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            width: 1,
-          }}
-        >
-          <Image src="/programmer.svg" alt="Login" width={300} height={140} />
-          <Typography
-            variant="h4"
-            color="primary"
-            align="center"
-            fontWeight="bold"
-            sx={{ paddingBottom: '1rem' }}
-          >
-            Desculpe, mas ainda não mapeamos essa área!
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            width: 1,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            marginTop: '5vh',
-          }}
-        >
-          <Typography
-            variant="h5"
-            color="textSecondary"
-            textAlign="center"
-            fontWeight="bold"
-            sx={{ paddingBottom: '1.5rem' }}
-          >
-            Tome um café e volte à navegação ☕
-          </Typography>
-          <Link href="/">
-            <Box
-              sx={{
-                bgcolor: '#3e6cd8',
-                color: '#ffffff',
-                fontWeight: 'bold',
-                display: 'flex',
-                width: 1,
-                justifyContent: 'center',
-                padding: '0.5rem',
-                borderRadius: '5px',
-                WebkitBorderRadius: '5px',
-                MozBorderRadius: '5px',
-                msBorderRadius: '5px',
-                OBorderRadius: '5px',
-                fontSize: '1.8rem',
-              }}
-            >
-              Voltar
-            </Box>
-          </Link>
-        </Box>
+    <Box sx={styles.container}>
+      <Image src="/programmer.svg" alt="Login" width={300} height={140} />
+      <Box>
+        <Typography variant="h4" color="#183883">
+          Desculpe, mas ainda não mapeamos essa área!
+        </Typography>
+        <Typography variant="h5" color="textSecondary">
+          Tome um café e volte à navegação ☕
+        </Typography>
       </Box>
+      <Button sx={styles.button}>
+        <Link href="/">Voltar</Link>
+      </Button>
     </Box>
   )
+}
+const styles = {
+  container: {
+    ...flexCenterContent,
+    width: 1,
+    height: '100vh',
+    flexDirection: 'column',
+    gap: 3,
+    '& .MuiTypography-root': {
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+  },
+  button: {
+    bgcolor: '#183883',
+    color: '#ffffff',
+    fontWeight: 'bold',
+    display: 'flex',
+    width: 1,
+    maxWidth: 300,
+    '&:hover': {
+      bgcolor: '#2f4c8f',
+    },
+  },
 }
