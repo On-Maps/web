1228d565012495de2d3eff123a9e39dbff4ee1e1
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-03-24 18:19:23 -0300

    feat/add global component campus

---
+++ src/components/SelectCampus/SelectCampus.component.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { ToggleButton, ToggleButtonGroup, ToggleButtonGroupProps, ToggleButtonProps } from "@mui/material";
+
+interface ISelectCampusProps {
+  value: string | null;
+  setValue: React.Dispatch<React.SetStateAction<any>>;
+  buttonGroupProps?: Omit<ToggleButtonGroupProps, 'value' | 'onChange'>;
+  buttonProps?: Omit<ToggleButtonProps, 'value' | 'onChange'>;
+}
+
+export function SelectCampus({ value, setValue, buttonGroupProps, buttonProps }: ISelectCampusProps) {
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setValue(newAlignment);
+  };
+
+  return (
+    <>
+      <ToggleButtonGroup
+        color="primary"
+        value={value}
+        exclusive
+        onChange={handleChange}
+        {...buttonGroupProps}
+      >
+        <ToggleButton {...buttonProps} value="cornelio">Cornélio Procópio</ToggleButton>
+        <ToggleButton {...buttonProps} value="bandeirantes">Bandeirantes</ToggleButton>
+        <ToggleButton {...buttonProps} value="jacarezinho">Jacarezinho</ToggleButton>
+      </ToggleButtonGroup>
+    </>
+  )
+}
