2eb5b8c774c57f873687f3414b55095ec1212476
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-03-28 21:14:05 -0300

    feat/cards no search atualizados e arrumados

--- src/pages/place/list/components/_PlaceCardSearch.component.tsx
+++ src/pages/place/list/components/_PlaceCardSearch.component.tsx
@@ -1,11 +1,11 @@
 import React, { createElement } from 'react'
 import Link from 'next/link'
-import styles from '../styles/PlaceCardSearch.module.css'
 import { DataMapCategories } from '@/data'
 import { AiFillStar, AiOutlineRight } from 'react-icons/ai'
 import { characterLimit } from '@/utils/functions'
 import Image from 'next/image'
 import { Box } from '@mui/material'
+import { flexCenterContent } from "@/utils/cssInJsBlocks";
 
 type TProps = {
   room: {
@@ -17,11 +17,12 @@ type TProps = {
     link: string
     latitude: number
     longitude: number
+    bloco: string
   }
   type?: 'grid' | 'list' | null
 }
 
-export function PlaceCardSearch({ room, type = 'list', ...rest }: TProps) {
+export function PlaceCardSearch({ room, type = 'list' }: TProps) {
   const icon =
     DataMapCategories.find((item) => item.value === room.category)?.icon ||
     AiFillStar
@@ -33,55 +34,93 @@ export function PlaceCardSearch({ room, type = 'list', ...rest }: TProps) {
   if (!type || type == null) return <div></div>
 
   return (
-    <Box sx={{
-      width: 1,
-    }}>
+    <Box sx={styles.containerMain}>
       {type === 'list' ? (
-        <Link href={`/place/${room.id}`} className={styles.link}>
-          <div className={styles.container}>
+        <Link href={`/place/${room.id}`}>
+          <Box sx={styles.cardListContainer}>
             {createElement(icon, {
               size: 20,
               color: 'black',
             })}
-            <div className={styles.text}>
-              <h3 className={styles.title}>{room.name}</h3>
-              <span className={styles.subtitle}>
-                Tipo: {room.category} | Piso: {room.piso}
+            <Box sx={styles.cardListStyle}>
+              <h3>{room.name}</h3>
+              <span>
+                Tipo: {room.category} | Piso: {room.piso} | Bloco: {room.bloco}
               </span>
-            </div>
+            </Box>
             <AiOutlineRight size={20} color="black" />
-          </div>
+          </Box>
         </Link>
       ) : (
         <Link href={`/place/${room.id}`}>
-          <div className={styles.containerGrid}>
-            <div className={styles.text}>
-              <h3 className={styles.title}>
-                {createElement(icon, {
-                  size: 16,
-                  color: 'black',
-                })}
+          <Box sx={styles.cardListContainerDetail}>
+            <Box sx={styles.imageStyle}>
+              <Image src={room.link} alt="logo" height={80} width={60} />
+            </Box>
+            <Box sx={styles.cardListDetailsStyle}>
+              <h3>
                 {room.name}
               </h3>
-              <div>
-                <p
-                  className={`${styles.subtitle} ${styles.container_subtitle}`}
-                >
-                  <span>Tipo: {room.category}</span>
-                  <span>Piso: {room.piso}</span>
-                  <span></span>
-                </p>
-                <p className={styles.subtitle}>
+              <Box sx={{ display: 'flex', gap: 4, mr: 4 }}>
+                <Box>
+                  <p>Tipo: {room.category}</p>
+                  <p>Piso: {room.piso}</p>
+                </Box>
+                <Box>
                   {characterLimit(room.description, 75)}
-                </p>
-              </div>
-            </div>
-            <div className={styles.image}>
-              <Image src={room.link} alt="logo" height={80} width={60} />
-            </div>
-          </div>
+                  <p>Bloco: {room.bloco}</p>
+                </Box>
+                <AiOutlineRight size={20} color="black" />
+              </Box>
+            </Box>
+          </Box>
         </Link>
       )}
     </Box>
   )
+}
+
+const styles = {
+  containerMain: {
+    width: 'fit-content',
+    height: 'fit-content',
+  },
+  cardListContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+    backgroundColor: 'white',
+    mb: 4,
+    p: 2,
+    borderRadius: 4,
+  },
+  cardListContainerDetail: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+    backgroundColor: 'white',
+    mb: 4,
+    p: 0,
+    borderRadius: 4,
+    with: '100%',
+  },
+  cardListStyle: {
+    ...flexCenterContent,
+    gap: 4,
+  },
+  cardListDetailsStyle: {
+    dispaly: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    borderRadius: 4,
+    overflow: 'hidden',
+    width: 60,
+    height: 80,
+  }
 }
--- src/pages/place/list/index.tsx
+++ src/pages/place/list/index.tsx
@@ -1,6 +1,6 @@
 import { SelectCampus } from "@/components"
 import React, { useState } from "react";
-import { Box, SxProps, TextField, Theme } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { flexCenterContent } from "@/utils/cssInJsBlocks";
 import WindowIcon from '@mui/icons-material/Window';
@@ -15,6 +15,7 @@ const rooms = [
     name: 'Sala 1',
     description: 'Sala de aula',
     piso: 1,
+    bloco: 'CCT',
     category: 'sala',
     link: 'https://cdn.discordapp.com/attachments/771470980324524043/1001706440601370766/preview1.png',
     latitude: -23.108,
@@ -25,6 +26,7 @@ const rooms = [
     name: 'Sala 2',
     description: 'Sala de aula',
     piso: 1,
+    bloco: 'CCT',
     category: 'sala',
     link: 'https://cdn.discordapp.com/attachments/771470980324524043/1074461555904745524/f53137d8-2472-434b-8ab0-385a611f0c8d.JPG',
     latitude: -23.108,
@@ -35,6 +37,7 @@ const rooms = [
     name: 'Sala 3',
     description: 'Sala de aula',
     piso: 2,
+    bloco: 'CCT',
     category: 'sala',
     link: 'https://cdn.discordapp.com/attachments/1087523573289189476/1087888499216224397/image.png',
     latitude: -23.108,
@@ -45,44 +48,42 @@ const rooms = [
 export default function PlaceList() {
   const [campus, setCampus] = React.useState(null);
   const [typeCard, setTypeCard] = useState<'grid' | 'list' | null>(null)
+  const customPlaceholderInput = (campus: string) => {
+    switch (campus) {
+      case 'cornelio':
+        return 'Pesquise por Cornélio Procópio'
+      case 'bandeirantes':
+        return 'Pesquise por Bandeirantes'
+      case 'jacarezinho':
+        return 'Pesquise por Jacarezinho'
+      default:
+        return 'Selecione um campus'
+    }
+  }
   return (
     <Box>
-      <Box sx={{ ...flexCenterContent, m: 4 }}>
+      <Box sx={styles.containerCampus}>
         <SelectCampus
           value={campus}
           setValue={setCampus}
           buttonProps={{
-            sx: {
-              width: 1,
-              borderRadius: 2,
-              '&.Mui-selected': {
-                backgroundColor: 'primary.main',
-                color: 'primary.contrastText',
-              },
-              '&.Mui-selected:hover': {
-                backgroundColor: 'primary.dark',
-              },
-            }
+            sx: styles.selectCampus
           }}
         />
       </Box>
-      <Box sx={{ ...flexCenterContent, flexDirection: 'row' }}>
+      <Box sx={styles.subContainer}>
         <Image src="/search-animate.svg" width={500} height={500} alt={"search image"} />
-        <Box sx={{ ...flexCenterContent, flexDirection: 'column', gap: 4, marginBottom: 35 }}>
+        <Box sx={styles.searchMainContainer}>
           <h1>Descubra lugares, faça uma pesquisa!</h1>
-          <Box sx={{ ...flexCenterContent, flexDirection: 'row', width: '100%', gap: 4 }}>
+          <Box sx={styles.searchSubContainer}>
             <TextField
-              sx={{ height: 'auto' }}
+              sx={styles.textField}
               InputProps={{
                 endAdornment: <Search />,
               }}
               disabled={!campus}
               placeholder={
-                campus
-                  ? `Pesquise por ${campus === 'cornelio' ?
-                    'Cornélio Procópio' : campus === 'bandeirantes' ?
-                      'Bandeirantes' : 'Jacarezinho'}` :
-                  'Selecione um campus'
+                customPlaceholderInput(campus ? campus : 'default')
               }
             />
             {typeCard === 'list' ? (
@@ -95,35 +96,55 @@ export default function PlaceList() {
               />
             )}
           </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', mt: 4 }}>
+            {rooms.map((room) => {
+              return <PlaceCardSearch type={typeCard} room={room} key={room.id} />
+            })}
+          </Box>
         </Box>
       </Box>
-      {rooms.map((room) => {
-        return <PlaceCardSearch type={typeCard} room={room} key={room.id} />
-      })}
     </Box >
   )
 }
 
-const containerMain: SxProps<Theme> = {
+const styles = {
   containerMain: {
-    ...flexCenterContent, flexDirection: 'grid'
+    display: 'grid'
   },
-}
-
-const subContainer: SxProps<Theme> = {
+  containerCampus: {
+    ...flexCenterContent,
+    flexDirection: 'grid'
+  },
+  selectCampus: {
+    width: 1,
+    borderRadius: 2,
+    '&.Mui-selected': {
+      backgroundColor: 'primary.main',
+      color: 'primary.contrastText',
+    },
+    '&.Mui-selected:hover': {
+      backgroundColor: 'primary.dark',
+    },
+  },
   subContainer: {
-    ...flexCenterContent, flexDirection: 'column', gap: 4, p: 4
-  }
-}
-
-const searchContainer: SxProps<Theme> = {
-  searchContainer: {
-  }
-}
-
-const textFiled: SxProps<Theme> = {
-  textFiled: {
-    height: 'auto',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
     width: '80%',
+    justifyItems: 'center',
+  },
+  searchMainContainer: {
+    display: 'block',
+    flexDirection: 'column',
+    gap: 4,
+    mt: 4,
+  },
+  searchSubContainer: {
+    ...flexCenterContent,
+    flexDirection: 'row',
+    width: '100%',
+    gap: 4
+  },
+  textField: {
+    height: 'auto'
   }
 }
